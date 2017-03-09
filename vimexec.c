5159328c188d0830865f6e604e29ad3c9abc4e68
dr024625 <DR024625@WIN1551762.northamerica.cerner.net>
2017-03-09 13:41:13 -0600

    WIP: starting on stdin redirection

--- vimexec.c
+++ vimexec.c
@@ -6,16 +6,19 @@
 INT main() {
     INT iBytesRead, iBytesWritten = 0;
     CHAR wsInputBuffer[INPUT_BUFFER_SIZE];
-    HANDLE hTempFile;
+    HANDLE hTempFile, newstdin, write_stdin;
     CHAR szTempDirectoryPath[MAX_PATH];
     CHAR szTempFileName[MAX_PATH];
     CHAR szTempBatchFileName[MAX_PATH];
     CHAR szFullCommand[MAX_PATH+sizeof(VIM_EXE)];
     STARTUPINFO si;
+    SECURITY_ATTRIBUTES sa;
     PROCESS_INFORMATION pi;
     ZeroMemory( &si, sizeof(si) );
     si.cb = sizeof(si);
     ZeroMemory( &pi, sizeof(pi) );
+    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sa.bInheritHandle = TRUE;         //allow inheritable handles
     // If no piped input, then exit
     if(GetFileType(GetStdHandle(STD_INPUT_HANDLE))!= FILE_TYPE_PIPE){
         ExitProcess(1);
@@ -42,9 +45,19 @@ INT main() {
     } while(iBytesRead);
     CloseHandle(hTempFile);
     // Start vim with that unique file
-    SetStdHandle(STD_INPUT_HANDLE, CreateFileW(L"CONIN$", GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL));
-    si.dwFlags = STARTF_USESTDHANDLES;
-    si.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+    //SetStdHandle(STD_INPUT_HANDLE, CreateFileW(L"CONIN$", GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL));
+    //si.dwFlags = STARTF_USESTDHANDLES;
+    //si.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+    //si.dwFlags = DETACHED_PROCESS;
+    //si.dwFlags = CREATE_NEW_CONSOLE;
+     if (!CreatePipe(&newstdin,&write_stdin,&sa,0))   //create stdin pipe
+     {
+            printf( "Unable to create stdin pipe. \n", GetLastError() );
+           ExitProcess(1);
+     }
+     si.dwFlags = STARTF_USESTDHANDLES|STARTF_USESHOWWINDOW;
+     si.wShowWindow = SW_HIDE;
+     si.hStdInput = newstdin;
     // Build command path
     lstrcpy(szFullCommand, VIM_EXE);
     lstrcat(szFullCommand, szTempBatchFileName);
@@ -57,14 +70,14 @@ INT main() {
     CloseHandle( pi.hProcess );
     CloseHandle( pi.hThread );
     // Execute .bat file
-    if( !CreateProcess( NULL, szTempBatchFileName, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
-    {
-        printf( "Unable to create vim process. Is vim.exe in your path? \n", GetLastError() );
-        ExitProcess(1);
-    }
-    WaitForSingleObject( pi.hProcess, INFINITE );
-    CloseHandle( pi.hProcess );
-    CloseHandle( pi.hThread );
+    //if( !CreateProcess( NULL, szTempBatchFileName, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+    //{
+    //    printf( "Unable to create vim process. Is vim.exe in your path? \n", GetLastError() );
+    //    ExitProcess(1);
+    //}
+    //WaitForSingleObject( pi.hProcess, INFINITE );
+    //CloseHandle( pi.hProcess );
+    //CloseHandle( pi.hThread );
     //ShellExecute(GetConsoleWindow(), "open", szTempBatchFileName, NULL, NULL, SW_SHOWDEFAULT);
     ExitProcess(0);
 }

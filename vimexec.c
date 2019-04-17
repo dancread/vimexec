32a27c6ed31bd54e7495c00de8de083081411726
Daniel Read <Daniel.Read@cerner.com>
2019-04-17 18:16:10 -0500

    Uses CreateProcess to launch the editor. Hopefull this allows for generic editor usage in the future

--- vimexec.c
+++ vimexec.c
@@ -16,6 +16,9 @@ INT main() {
   CHAR szTempFileName[MAX_PATH];
   CHAR szTempBatchFileName[MAX_PATH];
   CHAR szFullCommand[MAX_PATH+sizeof(VIM_EXE)];
+  STARTUPINFO si;
+  SECURITY_ATTRIBUTES sa;
+  PROCESS_INFORMATION pi;
   // Get %TEMP% env variable
   GetTempPath(MAX_PATH, (LPSTR)&szTempDirectoryPath);
   // Create unique temp bat file
@@ -30,26 +33,61 @@ INT main() {
   // Write file header
   WriteFile(hTempFile, MSG_FILE_HEADER, lstrlen(MSG_FILE_HEADER), &iBytesWritten, NULL);
   // If piped input, then pipe it to the empty file
-  if(GetFileType(GetStdHandle(STD_INPUT_HANDLE)) == FILE_TYPE_PIPE){
+  if(GetFileType(GetStdHandle(STD_INPUT_HANDLE)) == FILE_TYPE_PIPE) {
     // Read stdin
-    do{
-       ReadFile(GetStdHandle(STD_INPUT_HANDLE), wsInputBuffer, INPUT_BUFFER_SIZE, &iBytesRead, NULL);
-       WriteFile(hTempFile, wsInputBuffer, iBytesRead, &iBytesWritten, NULL);
+    do {
+      ReadFile(GetStdHandle(STD_INPUT_HANDLE), wsInputBuffer, INPUT_BUFFER_SIZE, &iBytesRead, NULL);
+      WriteFile(hTempFile, wsInputBuffer, iBytesRead, &iBytesWritten, NULL);
     } while(iBytesRead);
-#ifdef VIM_CONSOLE // Using console vim
-  // Reset stdin to FILE_TYPE_CHAR for console vim
-  freopen("CONIN$", "r", stdin);
-  // Reset stdout for pdcurses
-  freopen("CONOUT$", "w", stdout);
-#endif
   }
   CloseHandle(hTempFile);
   // Start vim with that unique file
+#ifdef VIM_CONSOLE // Using console vim
+  // Reset  & reopen stdout for vim terminal
+  sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+  sa.bInheritHandle = TRUE;
+  sa.lpSecurityDescriptor = NULL;
+  freopen("CONOUT$", "w", stdout);
+  SetStdHandle(STD_OUTPUT_HANDLE,
+    CreateFileW(L"CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, &sa, OPEN_EXISTING, 0, 0));
+  ZeroMemory( &sa, sizeof(sa) );
+
+  // Reset stdin to FILE_TYPE_CHAR for console vim
+  freopen("CONIN$", "r", stdin);
+  sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+  sa.bInheritHandle = TRUE;
+  sa.lpSecurityDescriptor = NULL;
+  SetStdHandle(STD_INPUT_HANDLE,
+    CreateFileW(L"CONIN$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, &sa, OPEN_EXISTING, 0, 0));
+  ZeroMemory( &sa, sizeof(sa) );
+#endif
   // Build command path
   lstrcpy(szFullCommand, VIM_EXE);
   lstrcat(szFullCommand, szTempBatchFileName);
-  system(szFullCommand);
+  ZeroMemory( &si, sizeof(si) );
+  si.cb = sizeof(si);
+  ZeroMemory( &pi, sizeof(pi) );
+  sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+  ZeroMemory( &sa, sizeof(sa) );
+  if( !CreateProcess( NULL, szFullCommand, NULL, NULL, FALSE, CREATE_NEW_PROCESS_GROUP, NULL, NULL, &si, &pi)) {
+    printf( "Unable to create editor process: %d\n", GetLastError() );
+    ExitProcess(1);
+  }
+  WaitForSingleObject( pi.hProcess, INFINITE );
+  CloseHandle( pi.hProcess );
+  CloseHandle( pi.hThread );
   // Execute .bat file
-  system(szTempBatchFileName);
+  ZeroMemory( &si, sizeof(si) );
+  si.cb = sizeof(si);
+  ZeroMemory( &pi, sizeof(pi) );
+  sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+  //sa.bInheritHandle = TRUE;         //allow inheritable handles
+  if( !CreateProcess( NULL, szTempBatchFileName, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+    printf( "Unable to create batch process: %d\n", GetLastError() );
+    ExitProcess(1);
+  }
+  WaitForSingleObject( pi.hProcess, INFINITE );
+  CloseHandle( pi.hProcess );
+  CloseHandle( pi.hThread );
   ExitProcess(0);
 }
